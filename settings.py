91a2ecffec63498578d37fe48f4dfd8f3a17589a
scott <mic1028002@gmail.com>
2016-04-28 08:27:13 +0800

    update code

--- settings.py
+++ settings.py
@@ -8,8 +8,9 @@ REDIS_URL = 'redis://localhost:6379/1'
 # REDIS_DB = 3
 # REDIS_PASSWORD = 'very secret'
 
+# 監聽這幾個 Queue 估計順序就是優先執行權重！
 # Queues to listen on
-# QUEUES = ['high', 'normal', 'low']
+QUEUES = ['high', 'normal', 'low', 'default']
 
 # If you're using Sentry to collect your runtime exceptions, you can use this
 # to configure RQ for it in a single step
